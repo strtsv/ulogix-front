72756a8417c655cc20c8450da379427080c00c7e
jsusilo <enr@bk.ru>
2021-12-12 21:37:07 +0500

    news page

---
+++ src/pages/News.js
@@ -0,0 +1,9 @@
+lute" data-component="Header">
+ro context-dark" data-component="Intro">
+nent="About">
+ble-bg section-lg" data-component="Video">
+ht" data-component="Services">
+ data-component="Brands">
+ data-component="News">
+e" data-component="Responsibility">
+dark" data-component="Footer">
